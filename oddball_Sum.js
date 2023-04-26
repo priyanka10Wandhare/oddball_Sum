b48b1ac568ba045fb7229aa61bfe9567073625aa
Priyanka Wandhare <125762766+priyanka10Wandhare@users.noreply.github.com>
2023-04-27 01:25:47 +0530

    Update oddball_Sum.js

--- oddball_Sum.js
+++ oddball_Sum.js
@@ -1,2 +1,31 @@
 # oddball_Sum
 oddball sum is javaScript interview question the solved without using reduce function,where we have to sum all the odd numbers from given number for that we have to create a function
+
+//sum of all odd numbers without reduce :
+
+function oddball_sum(nums){
+	var sum=0;
+	for(var i=0;i<num.length;i++){
+
+	if(nums[i]%2!===0){
+	sum=nums[i]+sum;
+	}
+     
+     }
+    return sum;
+}
+ 
+ddball_sum=([1,2,3,4,5,6]);
+
+
+
+//sum of all odd nnumbers with reduce function
+
+function oddball_Sum(nums){
+	return nums.reduce(function(total,item){
+		if(item%2===1){
+			return total+=item;
+		}
+	return total;
+	});
+      }
